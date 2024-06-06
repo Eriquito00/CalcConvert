0ae45dc60d2bf332c4acfc9433bb90ae27a0c985
Eriquito00 <150047758+Eriquito00@users.noreply.github.com>
2024-06-06 22:45:33 +0200

    boton calc funciona pero no opera

--- scripts/calc.js
+++ scripts/calc.js
@@ -1,91 +1,91 @@
 function num1() {
-    var display = document.getElementById('display');
+    const display = document.getElementById('display');
     display.value = display.value + '1';
 }
 
 function num2() {
-    var display = document.getElementById('display');
+    const display = document.getElementById('display');
     display.value = display.value + '2';
 }
 
 function num3() {
-    var display = document.getElementById('display');
+    const display = document.getElementById('display');
     display.value = display.value + '3';
 }
 
 function num4() {
-    var display = document.getElementById('display');
+    const display = document.getElementById('display');
     display.value = display.value + '4';
 }
 
 function num5() {
-    var display = document.getElementById('display');
+    const display = document.getElementById('display');
     display.value = display.value + '5';
 }
 
 function num6() {
-    var display = document.getElementById('display');
+    const display = document.getElementById('display');
     display.value = display.value + '6';
 }
 
 function num7() {
-    var display = document.getElementById('display');
+    const display = document.getElementById('display');
     display.value = display.value + '7';
 }
 
 function num8() {
-    var display = document.getElementById('display');
+    const display = document.getElementById('display');
     display.value = display.value + '8';
 }
 
 function num9() {
-    var display = document.getElementById('display');
+    const display = document.getElementById('display');
     display.value = display.value + '9';
 }
 
 function num0() {
-    var display = document.getElementById('display');
+    const display = document.getElementById('display');
     display.value = display.value + '0';
 }
 
 function coma() {
-    var display = document.getElementById('display');
+    const display = document.getElementById('display');
     display.value = display.value + ',';
 }
 
 function suma() {
-    var display = document.getElementById('display');
+    const display = document.getElementById('display');
     display.value = display.value + '+';
 }
 
 function rest() {
-    var display = document.getElementById('display');
+    const display = document.getElementById('display');
     display.value = display.value + '-';
 }
 
 function mult() {
-    var display = document.getElementById('display');
+    const display = document.getElementById('display');
     display.value = display.value + 'x';
 }
 
 function divi() {
-    var display = document.getElementById('display');
+    const display = document.getElementById('display');
     display.value = display.value + '÷';
 }
 
 function porc() {
-    var display = document.getElementById('display');
+    const display = document.getElementById('display');
     display.value = display.value + '%';
 }
 
 function borranum() {
     var display = document.getElementById('display');
-    display.value = display.value + 'borra numero';
+    display.value = display.value.slice(0, -1);
 }
 
 function borrartodo() {
     var display = document.getElementById('display');
-    display.value = display.value + 'borra todo';
+    display.value = '';
 }
 
 function result() {
