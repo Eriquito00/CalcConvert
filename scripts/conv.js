14a6e1e4209819809b4089a5114db68cac1c5242
Eriquito00 <150047758+Eriquito00@users.noreply.github.com>
2024-09-29 20:59:50 +0200

    conv JS

---
+++ scripts/conv.js
@@ -0,0 +1,47 @@
+// Factores de conversión respecto a la unidad base (metros)
+const factoresDistancia = {
+    km: 1000,
+    hm: 100,
+    dam: 10,
+    m: 1,
+    dm: 0.1,
+    cm: 0.01,
+    mm: 0.001,
+    legua: 4828.032,
+    milla: 1609.34,
+    furlong: 201.168,
+    cadena: 20.1168,
+    rod: 5.0292,
+    yarda: 0.9144,
+    pie: 0.3048,
+    pulgada: 0.0254,
+    micrometro: 0.000001
+};
+const factoresPeso = {
+    kg: 1,
+    hg: 0.1,
+    dag: 0.01,
+    g: 0.001,
+    dg: 0.0001,
+    cg: 0.00001,
+    mg: 0.000001,
+    libra: 0.453592,
+    onza: 0.0283495
+};
+const factoresTemperatura = {
+    celsius: 1,
+    fahrenheit: 33.8,
+    kelvin: 274.15 
+}
+const factoresDinero = {
+    usd: 1.12,
+    eur: 1,
+    jpy: 158.80,
+    gbp: 0.84,
+    aud: 1.61,
+    cad: 1.51,
+    chf: 0.94,
+    cnh: 7.80,
+    hkd: 8.68,
+    nzd: 1.76
+}
